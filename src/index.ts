33287026d2004ab8dc615680129c1c7b138f17a4
barclayd <barclaysd@me.com>
2024-10-28 00:59:10 +0000

    deploy: gcp terraform

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { simulateMission } from './mars';
 import { getCommandLineArgs } from './utils/bun';
+import { measurePerformance } from './utils';
 
 const { filePath, isDev } = getCommandLineArgs();
 
-simulateMission(filePath ?? Bun.env.DEFAULT_INPUT_FILE_PATH, isDev);
+measurePerformance(
+  () => simulateMission(filePath ?? Bun.env.DEFAULT_INPUT_FILE_PATH, isDev),
+  'Mission Simulation Complete 🚀',
+);
